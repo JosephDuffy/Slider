bd91d62a6b6340ddf01b4dd9772da513f8ec5acb
Joseph Duffy <commits@josephduffy.co.uk>
2019-07-18 11:43:24 +0100

    Initial Commit

---
+++ SliderPlayground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SliderPlayground
+//
+//  Created by Joseph Duffy on 18/07/2019.
+//  Copyright © 2019 Joseph Duffy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
