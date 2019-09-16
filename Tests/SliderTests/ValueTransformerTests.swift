62fc8f18dd27c057e247817de90054b01e72ce23
Joseph Duffy <commits@josephduffy.co.uk>
2019-09-16 14:32:56 +0100

    Use consistent minimum accuracy in tests

--- Tests/SliderTests/ValueTransformerTests.swift
+++ Tests/SliderTests/ValueTransformerTests.swift
@@ -3,22 +3,24 @@ import XCTest
 
 final class ValueTransformerTests: XCTestCase {
 
+    private let minimumAccuracy: Float = 0.00001
+
     func testMinimumAndMaximumPercent() {
         var valueTransformer = ValueTransformer(internalValue: 50, scaling: .linear(0...100))
-        XCTAssertEqual(valueTransformer.value(for: .internal), 50, accuracy: 0.1)
-        XCTAssertEqual(valueTransformer.value(for: .external), 50, accuracy: 0.1)
+        XCTAssertEqual(valueTransformer.value(for: .internal), 50, accuracy: minimumAccuracy)
+        XCTAssertEqual(valueTransformer.value(for: .external), 50, accuracy: minimumAccuracy)
 
         valueTransformer.maximumPercent = 40
-        XCTAssertEqual(valueTransformer.value(for: .internal), 40, accuracy: 0.1)
-        XCTAssertEqual(valueTransformer.value(for: .external), 40, accuracy: 0.1)
+        XCTAssertEqual(valueTransformer.value(for: .internal), 40, accuracy: minimumAccuracy)
+        XCTAssertEqual(valueTransformer.value(for: .external), 40, accuracy: minimumAccuracy)
 
         valueTransformer.maximumPercent = 100
-        XCTAssertEqual(valueTransformer.value(for: .internal), 40, accuracy: 0.1)
-        XCTAssertEqual(valueTransformer.value(for: .external), 40, accuracy: 0.1)
+        XCTAssertEqual(valueTransformer.value(for: .internal), 40, accuracy: minimumAccuracy)
+        XCTAssertEqual(valueTransformer.value(for: .external), 40, accuracy: minimumAccuracy)
 
         valueTransformer.minimumPercent = 60
-        XCTAssertEqual(valueTransformer.value(for: .internal), 60, accuracy: 0.1)
-        XCTAssertEqual(valueTransformer.value(for: .external), 60, accuracy: 0.1)
+        XCTAssertEqual(valueTransformer.value(for: .internal), 60, accuracy: minimumAccuracy)
+        XCTAssertEqual(valueTransformer.value(for: .external), 60, accuracy: minimumAccuracy)
     }
 
 }
